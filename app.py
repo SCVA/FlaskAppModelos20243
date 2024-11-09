0729bbcac6a3b315852fd608172fa50ba3926f54
Sebastian Camilo Vanegas Ayala <scva9708@gmail.com>
2024-11-09 08:58:59 -0500

    Se quitaron archivos no necesarios

--- app.py
+++ app.py
@@ -16,27 +16,6 @@ from flask import (Flask, redirect, render_template, request,
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
 def obtenerScraping():
     url = "https://web.archive.org/web/20241107001323/https://www.scimagojr.com/journalrank.php?page=2&total_size=29165"
     req = requests.get(url)
@@ -49,7 +28,7 @@ def obtenerScraping():
     X = X[:, np.newaxis]
     return X
 
-@app.route("/prueba")
+@app.route('/')
 def prueba():
     X = obtenerScraping()
     """
